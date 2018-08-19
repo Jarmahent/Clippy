1f9cad26955a3f0bbb0b8f2c57832a99400621a8
jarmahent <kjh7796@gmail.com>
2018-08-19 14:14:46 -0400

    db testing

---
+++ app/clipboard-data/dbHandler.js
@@ -0,0 +1,12 @@
+// let Database = require('better-sqlite3');
+//
+// let db = new Database('./copydata.db'); //Make sure to have the location of the db relative to the main.dev.js file
+
+// INSERT INTO copyData VALUES (1, "datahere", "datehere")
+// Use exec to write data nd use prepare to readt data
+
+// console.log(db.exec("INSERT INTO copyData (data, date) VALUES ('javascriptdata', 'javascriptdate')"))
+// console.log(db.prepare("SELECT * FROM copyData").get("id"))
+
+// db.exec()
+// db.close()
