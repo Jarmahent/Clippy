ac8e80ede97192bfcb908d0948796fee646b070e
jarmahent <kjh7796@gmail.com>
2018-08-25 04:39:19 -0400

    Full working now

--- app/components/Clippy.jsx
+++ app/components/Clippy.jsx
@@ -4,11 +4,8 @@ import { ipcRenderer } from 'electron';
 import styles from './Clippy.css';
 
 export default class Clippy extends Component {
-  state = {
-    clipArray: []
-  };
-
-  componentDidMount() {
+  constructor() {
+    super();
     ipcRenderer.send('db-init', 1);
     ipcRenderer.on('db-init', (event, args) => {
       const copyArray = [];
@@ -23,29 +20,33 @@ export default class Clippy extends Component {
     });
   }
 
+  state = {
+    clipArray: []
+  };
+
   componentDidUpdate() {
     console.log('Updated!');
-  }
-
-  render() {
-    /* eslint-disable */
     ipcRenderer.once('db-ch', (event, args) => {
       this.setState(prevState => ({
         clipArray: [...prevState.clipArray, args]
       }));
     });
+  }
+
+  render() {
+    let reversedArray = this.state.clipArray.reverse();
 
     return (
       <div className={styles.container} data-tid="container">
         <div className={styles.title}>Recent Copies</div>
         <div className={styles.copyList}>
-          <ol>
-            {this.state.clipArray.map((name, index) => (
+          <ul>
+            {reversedArray.map((name, index) => (
               <li className={styles.copies} key={index}>
                 {name}
               </li>
             ))}
-          </ol>
+          </ul>
         </div>
       </div>
     );
