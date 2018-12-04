e8af6635878f0cdbad2765e740b8ca34b8b89596
Kevin Hernandez <kjh7796@gmail.com>
2018-12-04 15:50:44 -0800

    Redoing settings UI

--- app/components/Settings.jsx
+++ app/components/Settings.jsx
@@ -4,7 +4,7 @@ import React, { Component } from 'react';
 // import { Link } from 'react-router-dom';
 import { ipcRenderer } from 'electron';
 // import routes from '../constants/routes.json';
-// import styles from './Settings.css';
+import styles from './Settings.css';
 import photonStyles from './Photon/css/photon.css';
 
 export default class Settings extends Component<Props> {
@@ -36,14 +36,75 @@ export default class Settings extends Component<Props> {
               photonStyles['toolbar-header']
             ].join(' ')}
           >
+            <span
+              className={[
+                photonStyles.icon,
+                photonStyles['icon-back'],
+                styles.backIcon
+              ].join(' ')}
+            />
             Settings
           </header>
+          <div className={styles.mainContainer}>
+            <button
+              type="button"
+              className={[
+                photonStyles.btn,
+                photonStyles['btn-large'],
+                photonStyles['btn-default']
+              ].join(' ')}
+            >
+              Erase Clipboard History
+            </button>
+            <button
+              type="button"
+              className={[
+                photonStyles.btn,
+                photonStyles['btn-large'],
+                photonStyles['btn-default']
+              ].join(' ')}
+            >
+              Open Images Folder
+            </button>
+            <button
+              type="button"
+              className={[
+                photonStyles.btn,
+                photonStyles['btn-large'],
+                photonStyles['btn-default']
+              ].join(' ')}
+            >
+              Check Connection
+            </button>
+            <button
+              type="button"
+              className={[
+                photonStyles.btn,
+                photonStyles['btn-large'],
+                photonStyles['btn-default']
+              ].join(' ')}
+            >
+              Erase Image History
+            </button>
+          </div>
+          <div className={styles.seconContainer}>
+            <div className={styles.tokenTitle}>Authentication Token</div>
+            <div className={photonStyles['form-group']}>
+              <input
+                type="text"
+                className={photonStyles['form-control']}
+                placeholder="Token"
+              />
+            </div>
+          </div>
           <footer
             className={[
               photonStyles.toolbar,
               photonStyles['toolbar-footer']
             ].join(' ')}
-          />
+          >
+            <div>Clippy v0.83.8</div>
+          </footer>
         </div>
       </div>
     );
