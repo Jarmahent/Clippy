b3dae731329d367cff8b373dcc999667afb03e8c
Kevin Hernandez <kjh7796@gmail.com>
2018-12-03 19:29:45 -0800

    Redoing UI using Photon

--- app/components/Settings.jsx
+++ app/components/Settings.jsx
@@ -1,11 +1,11 @@
 // @flow
 import React, { Component } from 'react';
-// import { ipcRenderer} from 'electron';
-import FontAwesome from 'react-fontawesome';
-import { Link } from 'react-router-dom';
+
+// import { Link } from 'react-router-dom';
 import { ipcRenderer } from 'electron';
-import routes from '../constants/routes.json';
-import styles from './Settings.css';
+// import routes from '../constants/routes.json';
+// import styles from './Settings.css';
+import photonStyles from './Photon/css/photon.css';
 
 export default class Settings extends Component<Props> {
   componentWillUnmount() {
@@ -28,60 +28,22 @@ export default class Settings extends Component<Props> {
 
   render() {
     return (
-      <div className={styles.container}>
-        <div className={styles.title}>
-          <Link to={routes.CLIPPY} className={styles.arrowContainer}>
-            <FontAwesome className={styles.arrow} name="arrow-left" size="2x" />
-          </Link>
-          <h2>Settings</h2>
-        </div>
-        <div className={styles.settingsArea}>
-          <div className={styles.tableSettings}>
-            <div className={styles.tableSettingsContent}>
-              Table Settings
-              <button
-                type="button"
-                className={styles.item}
-                onClick={this.clearDB}
-              >
-                ERASE HISTORY
-                <FontAwesome
-                  className={styles.danger}
-                  name="exclamation-triangle"
-                />
-              </button>
-              <button
-                type="button"
-                className={styles.item}
-                onClick={this.openImageDirectory}
-              >
-                Open Images Folder
-              </button>
-              <div className={styles.item}>
-                <button
-                  type="button"
-                  className={styles.item}
-                  onClick={this.clearImgDir}
-                >
-                  ERASE IMAGE HISTORY
-                  <FontAwesome
-                    className={styles.danger}
-                    name="exclamation-triangle"
-                  />
-                </button>
-              </div>
-            </div>
-          </div>
-          <div className={styles.tableSettings}>
-            <div className={styles.tableSettingsContent}>
-              Network Settings
-              <div>
-                <Link className={styles.item} to={routes.TOKEN}>
-                  Authentication
-                </Link>
-              </div>
-            </div>
-          </div>
+      <div>
+        <div className={photonStyles.window}>
+          <header
+            className={[
+              photonStyles.toolbar,
+              photonStyles['toolbar-header']
+            ].join(' ')}
+          >
+            Settings
+          </header>
+          <footer
+            className={[
+              photonStyles.toolbar,
+              photonStyles['toolbar-footer']
+            ].join(' ')}
+          />
         </div>
       </div>
     );
